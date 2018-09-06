6ad55774525bc1172247c6758fbf8e27ff2d3f6b
Piotr Dulikowski <piotr.dulikowski@ntti3.io>
2018-09-06 13:19:48 +0200

    More logs in PolicyValidate

--- schema/policy.go
+++ schema/policy.go
@@ -873,9 +873,11 @@ func addCustomFilters(schema *Schema, f map[string]interface{}, auth Authorizati
 
 //PolicyValidate validates api request using policy validation
 func PolicyValidate(action, path string, auth Authorization, policies []*Policy) (foundPolicy *Policy, foundRole *Role) {
+	log.Debug("PolicyValidate: %s, %s, %#v, %v", action, path, auth, policies)
 	if auth.IsAdmin() {
 		policies = append(policies, adminPolicy)
 	}
+	log.Debug("PolicyValidate: policy is admin? %b", auth.IsAdmin())
 	for _, policy := range policies {
 		if role := policy.match(action, path, auth); role != nil {
 			if policy.IsDeny() {
