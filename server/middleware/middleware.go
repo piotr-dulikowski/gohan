b46332bf3b0d711a38b3f9d2874f6736f0d11660
Piotr Dulikowski <piotr.dulikowski@ntti3.io>
2019-01-18 19:50:40 +0100

    Add support for providing Auth-Token cookie

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -90,6 +90,7 @@ func Logging() martini.Handler {
 		log.Info("[%s] Started %s %s for client %s data: %s",
 			requestContext["trace_id"], req.Method, req.URL.String(), addr, string(reqData))
 		log.Debug("[%s] Request headers: %v", requestContext["trace_id"], filterHeaders(req.Header))
+		log.Debug("[%s] Request cookies: %v", requestContext["trace_id"], filterCookies(req.Cookies()))
 		log.Debug("[%s] Request body: %s", requestContext["trace_id"], string(reqData))
 
 		rh := newResponseHijacker(rw.(martini.ResponseWriter))
@@ -123,11 +124,27 @@ func Metrics() martini.Handler {
 func filterHeaders(headers http.Header) http.Header {
 	filtered := http.Header{}
 	for k, v := range headers {
-		if k == "X-Auth-Token" {
+		if k == "X-Auth-Token" || k == "Cookie" {
 			filtered[k] = []string{"***"}
 			continue
 		}
 		filtered[k] = v
+	}
+
+	return filtered
+}
+
+func filterCookies(cookies []*http.Cookie) []*http.Cookie {
+	filtered := []*http.Cookie{}
+	for _, cookie := range cookies {
+		if cookie.Name == "Auth-Token" {
+			newCookie := &http.Cookie{}
+			*newCookie = *cookie
+			cookie = newCookie
+
+			cookie.Value = "***"
+		}
+		filtered = append(filtered, cookie)
 	}
 	return filtered
 }
@@ -318,6 +335,11 @@ func Authentication() martini.Handler {
 		}
 
 		authToken := req.Header.Get("X-Auth-Token")
+		if authToken == "" {
+			if authTokenCookie, err := req.Cookie("Auth-Token"); err == nil {
+				authToken = authTokenCookie.Value
+			}
+		}
 
 		var targetIdentityService IdentityService
 
